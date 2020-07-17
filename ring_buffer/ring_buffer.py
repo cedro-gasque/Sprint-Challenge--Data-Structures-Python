3ae9f7ede35a87f63c4e9b1c36a849196a4a4e44
Ramses Gasque <cedro.gasque@wsu.edu>
2020-07-17 09:29:33 -0700

    finished ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.buffer = []
+        self.capacity = capacity
 
     def append(self, item):
-        pass
-
+        node = {'value': item, 'age': self.capacity}
+        if len(self.buffer) == self.capacity:
+            for i in range(self.capacity):
+                if self.buffer[i]['age'] <= 0:
+                    self.buffer[i] = node
+                else:
+                    self.buffer[i]['age'] -= 1
+        else:
+            for i in range(self.capacity):
+                self.buffer[i]['age'] -= 1
+            self.buffer.append(node)
     def get(self):
-        pass
+        return [node['value'] for node in self.buffer]
