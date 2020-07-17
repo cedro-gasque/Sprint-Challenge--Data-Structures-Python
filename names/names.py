4884321f87b6d4e32815645c1c3ade2e1bc0b9d4
Ramses Gasque <cedro.gasque@wsu.edu>
2020-07-17 10:49:35 -0700

    finished stretch goal (not a very good implementation and is probably slower than list comp lol)

--- names/names.py
+++ names/names.py
@@ -22,3 +22,22 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+start_time = time.time()
+duplicates = set(names_1).intersection(set(names_2))
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+# list version for stretch goal
+def divide_and_conquer(value):
+    global names_2
+    l = len(value)
+    if l == 1:
+        return value * (value[0] in names_2)
+    return (divide_and_conquer(value[0:l//2])
+    + divide_and_conquer(value[l//2:l]))
+start_time = time.time()
+duplicates = divide_and_conquer(names_1)
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
